4fde4bca13129009b277a11a936d8efdff36af9e
sombra-roman-strutynskyi <roman.strutynskyi@sombrainc.com>
2021-02-28 21:03:26 +0200

    rewrite logic for material modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,49 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyMaterialModule } from '@ngx-formly/material';
 import { FORMLY_CONFIG, SHARED_COMPONENTS } from './components';
 import { SHARED_DIRECTIVES } from './directives';
+import { MaterialModule } from './material';
 import { SHARED_PIPES } from './pipes';
 import { IconService, SHARED_SERVICES } from './services';
-
-const MATERIAL_MODULES = [
-  MatButtonModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatToolbarModule,
-  MatCardModule,
-  MatIconModule,
-  MatTabsModule,
-  MatCheckboxModule,
-  MatExpansionModule,
-  MatSnackBarModule,
-  MatProgressSpinnerModule,
-  MatListModule,
-  MatDialogModule,
-];
 
 export const SHARED_MODULES = [
   CommonModule,
   ReactiveFormsModule,
-  ...MATERIAL_MODULES,
+  MaterialModule,
 ];
-
 @NgModule({
   declarations: [...SHARED_DIRECTIVES, ...SHARED_COMPONENTS, ...SHARED_PIPES],
   imports: [
