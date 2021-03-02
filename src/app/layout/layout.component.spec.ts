da90351f1c429232802ab1d867d30c05f78fad8c
sombra-roman-strutynskyi <roman.strutynskyi@sombrainc.com>
2021-03-02 22:56:24 +0200

    add unit test to layout

--- src/app/layout/layout.component.spec.ts
+++ src/app/layout/layout.component.spec.ts
@@ -1,24 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { ROUTES_DATA } from '@shared';
+import { createComponent, TestingModule } from '@testing';
 
 import { LayoutComponent } from './layout.component';
+const imports = [TestingModule];
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LayoutComponent],
-    }).compileComponents();
-  });
+  let router: Router;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LayoutComponent);
+    fixture = createComponent<LayoutComponent>(LayoutComponent, [], imports);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should logout', () => {
+    const spy = jest.spyOn(component.logouted, 'emit');
+    component.logout();
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+  it('should redirect to profile', () => {
+    const spy = jest.spyOn(router, 'navigateByUrl');
+    component.goToEditUser();
+    expect(spy).toHaveBeenCalledWith(ROUTES_DATA.PROFILE.url);
+  });
+  it('should redirect to home', () => {
+    const spy = jest.spyOn(router, 'navigateByUrl');
+    component.goToHome();
+    expect(spy).toHaveBeenCalledWith('/');
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 });
