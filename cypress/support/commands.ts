983a0ec67c29745a11d835958a95125248d0c527
sombra-roman-strutynskyi <roman.strutynskyi@sombrainc.com>
2021-04-07 16:49:05 +0300

    login with wait

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,14 +1,11 @@
 import 'cypress-file-upload';
 
 Cypress.Commands.add('login', () => {
-  cy.intercept('post', '*identitytoolkit/v3/relyingparty/getAccountInfo?*').as(
-    'login'
-  );
   cy.visit('/').then(() => {
     cy.get('input[type="email"]').type(Cypress.env('userEmail'));
     cy.get('input[type="password"]').type(Cypress.env('userPassword'));
     cy.get('.form-buttons button[type="submit"]').click();
-    cy.wait('@login');
+    cy.wait(10000);
   });
 });
 Cypress.Commands.add('logout', () => {
